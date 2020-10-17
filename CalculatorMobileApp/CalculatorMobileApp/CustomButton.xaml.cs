7bb1916c0d2b707e2d8816392bac4845eb197ae6
Nikita Modelov <modelov.n.a@yandex.ru>
2020-10-18 03:29:11 +0700

    Добавьте файлы проекта.

---
+++ CalculatorMobileApp/CalculatorMobileApp/CustomButton.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CalculatorMobileApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CustomButton : ContentView
+    {
+        public static readonly BindableProperty CustomButtonContentProperty = BindableProperty.Create(
+            "ButtonContent",
+            typeof(string),
+            typeof(CustomButton),
+            string.Empty);
+
+        public static readonly BindableProperty CustomButtonColorTextProperty = BindableProperty.Create(
+            "ContentColor",
+            typeof(Color),
+            typeof(CustomButton));
+
+        public string ButtonContent
+        {
+            get => (string)GetValue(CustomButton.CustomButtonContentProperty);
+            set => SetValue(CustomButton.CustomButtonContentProperty, value);
+        }
+
+        public Color ContentColor
+        {
+            get => (Color)GetValue(CustomButtonColorTextProperty);
+            set => SetValue(CustomButtonColorTextProperty, value);
+        }
+
+        public CustomButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
